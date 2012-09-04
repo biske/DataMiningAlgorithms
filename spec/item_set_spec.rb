83abff38819b78a166ae9b8ff2678388029e587d
Ivan Bisevac <ivan.bisevac@gmail.com>
2012-09-05 01:16:33 +0200

    Tests passed for item_set

--- spec/item_set_spec.rb
+++ spec/item_set_spec.rb
@@ -1,17 +1,28 @@
 require_relative '../item_set'
 
 describe ItemSet do
-  it "count should return support count of this itemset in transactions" do
-    transactions = [["Chicken", "Clothes", "Milk"],
-                    ["Beef", "Chicken", "Milk"],
+  before(:all) do
+    @transactions = [["Beef", "Chicken", "Milk"],
                     ["Beef", "Cheese"],
                     ["Cheese", "Boots"],
                     ["Beef", "Chicken", "Cheese"],
                     ["Beef", "Chicken", "Clothes", "Cheese", "Milk"],
                     ["Chicken", "Clothes", "Milk"],
                     ["Chicken", "Milk", "Clothes"]]
+  end
+  it "count should return 4 support count for ['Beef']" do
     array = ["Beef"]
-    is = ItemSet.new(array, transactions, 0)
+    is = ItemSet.new(array, @transactions, 0)
     is.count.should eq(4)
+  end
+  it "count should return 3 support count for ['Beef', 'Chicken']" do
+    array = ["Beef", "Chicken"]
+    is = ItemSet.new(array, @transactions, 0)
+    is.count.should eq(3)
+  end
+  it "count should return 3 support count for ['Chicken', 'Clothes', 'Milk']" do
+    array = ['Chicken', 'Clothes', 'Milk']
+    is = ItemSet.new(array, @transactions, 0)
+    is.count.should eq(3)
   end
 end
